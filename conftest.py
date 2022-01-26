43bcac0e51f0966c40120b2a98085d56f10af254
Jim Brännlund <jimbrannlund@fastmail.com>
2022-01-26 15:58:37 +0100

    update README

--- conftest.py
+++ conftest.py
@@ -1,6 +1,124 @@
+import base64
 import os
 import pytest
+import random
+from py.xml import html
+from pytest_html import extras
+# from pytest_html.plugin import HTMLReport
+# print("TRHEE")
 
+
+def pytest_html_results_summary(prefix, summary, postfix):
+    prefix.extend(["<p>prefix</p>"])
+    summary.extend(["<p>summary</p>"])
+    postfix.extend(["<p>postfix</p>"])
+
+
+def pytest_html_results_table_header(cells):
+    # print("HEADERS: ", cells)
+    cells.insert(2, "<th>Two</th>")
+    cells.insert(3, "<th>Three</th>")
+
+    cells.insert(4, html.th("Four", class_="sortable", col='time'))
+    # cells[4] = html.th("Four", class_="sortable", col='time')
+    #cells.append("<th>Six</th>")
+    #cells.append("<th>Five</th>")
+
+
+def pytest_html_results_table_row(report, cells):
+    # print("ROWS: ", cells)
+    # if report.skipped:
+    #     del cells[:]
+    # else:
+    from time import time
+    cells.insert(2, f"<td>{report.nodeid}</td>")
+    cells.insert(3, f"<td>{report.nodeid}</td>")
+    cells.insert(4, html.td(time(), class_="col-time"))
+    # cells[4] = html.td("fofo", class_="col-time")
+    #cells.append(f"<td>meh</td>")
+    #cells.append(f"<td>moh</td>")
+
+
+# def pytest_html_results_table_html(report, data):
+    # print("REPORT: ", report)
+    # print("DATA:", data)
+    # print("TYPE: ", type(data))
+    # data.append(f"<p>{report.nodeid}blergh</p>")
+    # print("")
+    # print(len(data))
+    # for each in data:
+    #     print("================================")
+    #     print(str(each))
+    #     print(type(each))
+    # if report.passed:
+    #     del data[:]
+    #     data.append(f"<p>{report.nodeid}blergh</p>")
+    # print("DATA:", data[-1])
+    # print("TYPE: ", type(data[-1]))
+
+
+# def pytest_html_results_table_html(report, data):
+#     if report.when in ('call', 'teardown'):
+#         setup_text = None
+#         call_text = None
+#         other_text = ''
+#         for each in data:
+#             title, text = each.split("\n", 1)
+#             if 'Captured stdout setup' in title:
+#                 setup_text = text
+#             elif 'Captured stdout call' in title:
+#                 call_text = text
+#             else:
+#                 other_text += text
+#         if setup_text is None or len(setup_text) == 0:
+#             return  # ??
+#
+#         import uuid
+#         html = []
+#         node_id = uuid.uuid4() # random id
+#         html.append(f'<input id="{node_id}" class="toggle" type="checkbox">') # Setup checkbox
+#         html.append(f'<label for="{node_id}" class="lbl-toggle">Show setup output</label>') # Label
+#         html.append(f'<div class="log collapsible-content">{setup_text}</div>') # Setup log
+#         if call_text is not None and len(call_text):
+#             node_id = uuid.uuid4() # random id
+#             html.append(f'<input id="{node_id}" class="toggle" type="checkbox" checked>') # Call checkbox
+#             html.append(f'<label for="{node_id}" class="lbl-toggle">Show test output</label>') # Label
+#             html.append(f'<div class="log collapsible-content">{call_text}</div>') # Call log
+#         elif not len(other_text):
+#             # show setup if no call log and no other data
+#             html[0].replace('<input ', '<input checked ')
+#
+#         if len(other_text):
+#             html.append('<div class="log">'+other_text+'</div>')
+#         del data[:]
+#         data.append('<div>'+''.join(html)+'</div>')
+
+
+def pytest_html_report_title(report):
+    # print("inside hook")
+    report.title = "My very own title!"
+
+
+# @pytest.hookimpl(trylast=True)
+# def pytest_runtest_logreport(report):
+#     print("err: ", report.capstderr)
+#     print("out: ", report.capstdout)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == 'call':
+        report.extras = [extras.html('<div>sandman</div>')]
+        # content = "u'\u0081'"
+        # report.extras = [extras.text(f"{content}")]
+        # extra = getattr(report, "extra", [])
+        # content = str(random.random())
+        # charset = "utf-8"
+        # data = base64.b64encode(content.encode(charset)).decode(charset)
+        # extra.append(extras.mp4(f"{data}"))
+        # report.extra = extra
 
 # def pytest_html_results_table_header(cells):
 #     cells[1].attr.class_ = 'sortable initial-sort'
@@ -11,11 +129,11 @@ import pytest
 # def pytest_runtest_makereport(item, call):
 #     outcome = yield
 #     report = outcome.get_result()
-#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     pytest_html2 = item.config.pluginmanager.getplugin('html')
 #     extra = getattr(report, 'extra', [])
-
+#     print(pytest_html2)
 #     main_script_dir = os.path.dirname(__file__)
-#     rel_path = "screenshots/ss.jpg" # hardcoded image file
-#     image = pytest_html.extras.image(os.path.join(main_script_dir, rel_path))
+#     rel_path = "screenshots/ss.jpg"  # hardcoded image file
+#     image = pytest_html2.extras.image(os.path.join(main_script_dir, rel_path))
 #     extra.append(image)
 #     report.extra = extra
--- test_html.py
+++ test_html.py
@@ -1,7 +1,32 @@
-#import pytest
+import pytest
 from pytest_html import extras
+from time import sleep
+import logging
+from pic_b64 import PIC_AS_BASE64
+from selenium import webdriver
+
+# pytest.skip("skip marker module", allow_module_level=True)
 
 
+# def test_driver_screenshot():
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.add_argument("--window-size=1920x1080")
+#     driver = webdriver.Remote(
+#         command_executor="http://127.0.0.1:4444", options=chrome_options
+#     )
+#     try:
+#         driver.get("https://www.duckduckgo.com")
+#         result = driver.get_screenshot_as_base64()
+#         print(result)
+#     finally:
+#         driver.quit()
+
+# def test_driver_screenshot(driver):
+#     driver.get("https://www.duckduckgo.com")
+#     assert False
+
+
+# @pytest.mark.flaky(reruns=2)
 def test_url(extra):
     """
         bla bla bla bla
@@ -11,12 +36,37 @@ def test_url(extra):
     """
     # driver.get("https://www.google.com")
     extra.append(extras.text("some string"))
-    extra.append(extras.image("file:///Users/jimbrannlund/dev/pytest-dev/testing-html/screenshot.png"))
-    extra.append(extras.image("file:///Users/jimbrannlund/dev/pytest-dev/testing-html/screenshot.png"))
+    # extra.append(extras.text(b"\xe2\x80\x94"))
+    # extra.append(extras.text("—"))
+    # extra.append(extras.html("<div>Additional HTML</div>"))
+    extra.append(extras.url("https://example.com"))
+    extra.append(extras.json({"hello": "world"}))
+
+    # link (do nothing)
+    extra.append(extras.image("https://www.dailydot.com/wp-content/uploads/2018/05/Spiderman-Games.jpg"))
+    extra.append(extras.video("vid.mp4"))
+    # relative (copy to assets / base64)
+    extra.append(extras.image("screenshot1.png"))
+
+    extra.append(extras.html(r"<div>Additional HTML</div>"))
+    extra.append(extras.html(r"<div>Second HTML</div>"))
+
+    # absolute (copy to assets / base64)
+    extra.append(extras.image("file:///Users/jimbrannlund/dev/pytest/testing-html/screenshot2.png"))
+
+    # base 64
+    extra.append(extras.image(PIC_AS_BASE64))
+    sleep(0.2)
     assert False
 
 
-def test_url2():
+def test_url_2(extra):
+    extra.append(extras.image("https://www.dailydot.com/wp-content/uploads/2018/05/Spiderman-Games.jpg"))
+    sleep(0.3)
+    assert True
+
+
+def test_pass(extra):
     """
         bla bla bla bla
         alb alb alb
@@ -24,8 +74,57 @@ def test_url2():
         :param just
     """
     # driver.get("https://www.google.com")
+    extra.append(extras.text("bla bla bla"))
     assert True
 
+
+def test_skip():
+    pytest.skip("I DON*T NEED A REASON")
+
+
+def test_fail():
+    assert False
+
+
+def test_xfail():
+    pytest.xfail('MEH')
+
+
+@pytest.mark.xfail()
+def test_xpass():
+    pass
+
+
+@pytest.fixture
+def setup1():
+    hello
+    
+
+def test_setup(setup1):
+    assert True
+
+
+@pytest.fixture
+def setup2():
+    print("setup2")
+    yield
+    hello
+
+
+def test_teardown(setup2):
+    assert True
+
+
+@pytest.fixture
+def setup3():
+    sleep(2)
+    yield
+    sleep(2)
+
+
+def test_duration(setup3):
+    sleep(2)
+    assert True
 
 # def test_url3(driver, extra):
 #     """
@@ -38,3 +137,147 @@ def test_url2():
 #     driver.get_screenshot_as_file("screenshot.png")
 #     extra.append(extras.png("screenshot.png"))
 #     assert True
+
+
+@pytest.fixture
+def ansi_setup():
+    print("hello")
+    yield
+    print(bye)
+
+
+def test_ansi(ansi_setup):
+    logging.info("well well well")
+    colors = ['\033[31mRCOLOR\033[0m', '\033[32mGCOLOR\033[0m',
+              '\033[33mYCOLOR\033[0m']
+    for color in colors:
+        print(color)
+
+    assert False
+
+
+def test_time():
+    sleep(1)
+    assert True
+
+
+@pytest.fixture
+def setup4():
+    hello
+    yield
+    hello
+
+
+def test_setup_and_teardown(setup4):
+    assert True
+
+
+def test_capture_logging_fail(loggingfixture):
+    logging.info("failing")
+    logging.info("no")
+    assert False
+
+
+@pytest.fixture
+def loggingfixture():
+    logging.info("setup")
+    yield
+    logging.info("teardown")
+
+
+def test_capture_logging_pass(loggingfixture):
+    logging.info("passing")
+    logging.info("yes")
+    assert True
+
+
+@pytest.fixture
+def capsetup():
+    error
+
+
+def test_capture_setup_fail(capsetup):
+    assert True
+
+
+def test_capture_test_fail():
+    import sys
+    print("merror", file=sys.stderr)
+    print("bah humbug")
+    assert False
+
+
+@pytest.fixture
+def stdout():
+    import sys
+    print("I am setup")
+    print("setup error", file=sys.stderr)
+    #logging.info("setup")
+    yield
+    print("I am teardown")
+    print("teardown error", file=sys.stderr)
+    #logging.info("teardown")
+
+
+def test_capture_test_pass(stdout):
+    import sys
+    print("merror", file=sys.stderr)
+    #logging.info("fail fail")
+    print("bah humbug")
+    assert True
+
+
+@pytest.fixture
+def capteardown():
+    print("setup")
+    yield
+    error
+
+
+def test_capture_test_teardown(capteardown):
+    assert True
+
+
+def test_abc():
+    print("这是UTF-8内容")
+    assert False
+
+
+@pytest.mark.flaky(reruns=2)
+def test_rerun():
+    assert False
+
+
+@pytest.mark.flaky(reruns=2)
+def test_rerun_with_teardown(loggingfixture):
+    assert False
+
+
+import sys
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="skip marker")
+def test_skip_marker():
+    assert False
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="skip marker class")
+class TestSkip:
+    def test_skip_marker_class(self):
+        assert False
+
+
+@pytest.mark.xfail(reason="is broken")
+class TestXFail:
+    def test_xfail(self):
+        assert False
+
+
+@pytest.fixture
+def vegars_setup():
+    print("vegars setup")
+
+
+def test_vegars(vegars_setup):
+    print("vegars call")
+    assert True
